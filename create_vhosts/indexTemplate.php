93bf486bc500b4442b4240df853c8125b9866380
Eric H <38140317+xPand4B@users.noreply.github.com>
2019-03-12 20:45:49 +0100

    Update indexTemplate.php

--- create_vhosts/indexTemplate.php
+++ create_vhosts/indexTemplate.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>vHost</title>
+        <title>Welcome!</title>
     </head>
     <body>
         <style>
-            .errorScreen{
-                width: 100vw;
-                text-align: center;
-                color: #333;
-            }
-
-            h1{
-                font-size: 25vh;
-                margin-top: 20vh;
-                margin-bottom: 10vh;
-            }
+            body { text-align: center; padding: 150px;}
+            h1 { font-size: 50px; }
+            body { font: 20px Helvetica, sans-serif; color: #333; }
+            article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+            li { padding: 10px 0px; }
+            a { color: #dc8100; text-decoration: none; }
+            a:hover { color: #333; text-decoration: none; }
         </style>
-
-        <div class="errorScreen">
-            <h1>Ã’_Ã“</h1>
-        </div>    
+        
+        <article>
+            <h1>Welcome to this Site!</h1>
+            <div>
+                <p>Currently there is nothing to find here, but I&rsquo;m sure, that you can build something awesome!</p>
+                <p>If you need to you can always at <strong>xpand.4beatz@gmail.com</strong>.</p>
+                <hr>
+                    <ul>
+                        <li><a href="https://github.com/xPand4B" target="_blank">Github</a></li>
+                    </ul>
+                <hr>
+                <br>
+                <p>&mdash; xPand</p>
+            </div>
+        </article>
     </body>
 </html>
