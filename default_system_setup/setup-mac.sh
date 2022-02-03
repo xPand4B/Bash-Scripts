5dde5ba42b7d1218a435260a598799f45e5fb71b
Eric Heinzl <38140317+xPand4B@users.noreply.github.com>
2022-02-03 23:08:14 +0100

    Updated Mac setup

--- default_system_setup/setup-mac.sh
+++ default_system_setup/setup-mac.sh
@@ -28,12 +28,11 @@ brew install git
 echo -e "${green}Git successfully installed!${reset}"
 
 # Install ZSH
+# https://sourabhbajaj.com/mac-setup/iTerm/zsh.html
 #=================================
 echo ""
 echo -e "${green}Installing ZSH...${reset}"
 brew update
-
-# https://sourabhbajaj.com/mac-setup/iTerm/zsh.html
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # chsh -s $(which zsh)
@@ -55,22 +54,15 @@ brew update
 
 brew install php@7.4
 brew install php@8.0
+brew install php@8.1
 
 brew unlink php
 brew unlink php@7.4
 brew unlink php@8.0
+brew unlink php@8.1
+
 brew link php@8.0 --force --overwrite
 echo -e "${green}PHP sucessfully installed!${reset}"
-
-# Install Node
-#=================================
-echo ""
-echo -e "${green}Installing Node...${reset}"
-brew update
-
-brew install node
-
-echo -e "${green}Node sucessfully installed!${reset}"
 
 # Install Composer
 #=================================
@@ -78,21 +70,10 @@ echo ""
 echo -e "${green}Installing Composer...${reset}"
 brew update
 brew install composer
-
 echo -e "${green}Composer successfully installed${reset}"
 
-# Install CLI Helper
-#=================================
-echo ""
-echo -e "${green}Installing CLI Helper...${reset}"
-brew update
-
-brew install symfony-cli/tap/symfony-cli
-composer global require laravel/installer
-
-echo -e "${green}CLI Helper successfully installed${reset}"
-
-# Install Valet
+# Install Laravel Valet
+# https://laravel.com/docs/8.x/valet
 #=================================
 echo ""
 echo -e "${green}Installing Laravel Valet...${reset}"
@@ -102,6 +83,33 @@ valet install
 valet use php@8.0 --force
 echo -e "${green}Laravel Valet successfully installed!${reset}"
 
+# Install PHP Monitor
+# https://github.com/nicoverbruggen/phpmon
+#=================================
+echo ""
+echo -e "${green}Installing PHP Monitor...${reset}"
+brew update
+brew tap nicoverbruggen/homebrew-cask
+brew install --cask phpmon
+echo -e "${green}PHP Monitor successfully installed!${reset}"
+
+# Install CLI Helper
+#=================================
+echo ""
+echo -e "${green}Installing CLI Helper...${reset}"
+brew update
+brew install symfony-cli/tap/symfony-cli
+composer global require laravel/installer
+echo -e "${green}CLI Helper successfully installed${reset}"
+
+# Install Node
+#=================================
+echo ""
+echo -e "${green}Installing Node...${reset}"
+brew update
+brew install node
+echo -e "${green}Node sucessfully installed!${reset}"
+
 # Creating directories
 #=================================
 echo ""
@@ -109,7 +117,6 @@ echo -e "${green}Creating directories...${reset}"
 mkdir $developmentDir
 mkdir $developmentDir/00-Projects
 mkdir $developmentDir/Symfony
-mkdir $developmentDir/sw5
 mkdir $developmentDir/sw6
 mkdir $developmentDir/sw6_testing
 ls -la $developmentDir
@@ -119,18 +126,16 @@ echo -e "${green}Directories successfully created!${reset}"
 #=================================
 echo ""
 echo -e "${green}Cloning xPand4B/ValetDashboard...${reset}"
-cd $developmentDir/00-Projects
-git clone git@github.com:xPand4B/ValetDashboard.git
-cd ..
+git clone git@github.com:xPand4B/ValetDashboard.git $developmentDir/00-Projects/ValetDashboard
 echo -e "${green}xPand4B/ValetDashboard successfully cloned...${reset}"
 
 # Parking Directories to Valet
 #=================================
 echo ""
 echo -e "${green}Linking directories to Valet...${reset}"
-cd $developmentDir/00-Projects && valet park && cd ..
-cd $developmentDir/sw6_testing && valet park && cd ..
-cd $developmentDir/Symfony && valet park && cd ..
+cd $developmentDir/00-Projects && valet park
+cd $developmentDir/sw6_testing && valet park
+cd $developmentDir/Symfony && valet park
 echo -e "${green}Directories successfully linked!${reset}"
 
 
